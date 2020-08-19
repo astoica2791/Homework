7169377ae783080773bb567622d8cd972e5055f3
astoica2791 <andra.stoica@lseg.com>
2020-08-19 22:45:24 +0300

    Homework 1 Arrays

--- src/com/lseg/arrays/ARRAYSPLAY.java
+++ src/com/lseg/arrays/ARRAYSPLAY.java
@@ -2,7 +2,17 @@ package com.lseg.arrays;
 
 public class ARRAYSPLAY {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+            int d = 7;
+            int[] lista = {1,2,3,4,5};
+            int dif = d%lista.length;
+            for (int i=dif; i<lista.length;i++){
+                System.out.print(lista[i]+" ");
+            }
+            for(int j=0;j<dif;j++){
+                System.out.print(lista[j]+" ");
+            }
+
 
     }
 }
