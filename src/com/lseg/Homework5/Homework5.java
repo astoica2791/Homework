00909662ed7c1db320c5d09988d4459fe1662cf1
astoica2791 <andra.stoica@lseg.com>
2020-09-17 12:59:17 +0300

    Homework 5

--- src/com/lseg/Homework5/Homework5.java
+++ src/com/lseg/Homework5/Homework5.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Homework5 {
 
         public static void main(String[] args) {
-            //ex2();
-            //ex3();
+            ex2();
+            ex3();
             ex4();
 
         }
